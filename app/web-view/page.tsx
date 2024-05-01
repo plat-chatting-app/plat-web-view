3d593972d39cac2ee2295afdb9242fd328ab19d2
jinhyeok15 <ekfrhd97@gmail.com>
2024-05-01 19:40:26 +0900

    feat: device type middleware 추가

--- app/web-view/page.tsx
+++ app/web-view/page.tsx
@@ -1,6 +1,15 @@
 import { MapService, options } from '@usecase/map'
 
-const Page = () => {
+type PageProps = {
+  searchParams: {
+    viewport: 'mobile' | 'desktop'
+  }
+}
+
+const Page = ({ searchParams: { viewport } }: PageProps) => {
+  if (viewport === 'mobile') {
+    return '페이지 준비중입니다...'
+  }
   return <MapService options={options} />
 }
 
---
+++ middleware.ts
@@ -0,0 +1,15 @@
+import { NextRequest, NextResponse, userAgent } from 'next/server'
+
+export function middleware(request: NextRequest) {
+  const url = request.nextUrl
+  if (!url.searchParams.get('viewport')) {
+    const { device } = userAgent(request)
+    const viewport = device.type === 'mobile' ? 'mobile' : 'desktop'
+    url.searchParams.set('viewport', viewport ?? '')
+    return NextResponse.rewrite(url)
+  }
+}
+
+export const config = {
+  matcher: '/web-view/:path*',
+}
