2df7acd7289c32af62279dee2bbddfc55dc189b4
jinhyeok15 <ekfrhd97@gmail.com>
2024-05-04 15:42:40 +0900

    fix:test 주석 제거

--- usecase/map/MapService.tsx
+++ usecase/map/MapService.tsx
@@ -14,58 +14,48 @@ interface Props {
 }
 
 const MapService = ({ device, os }: Props) => {
-  return (
-    <SeeOther
-      title={
-        <Image
-          src="/static/plat-logo-nobg.svg"
-          alt="plat-logo"
-          width={40}
-          height={40}
-        />
-      }
-      description={
-        <span>
-          <p>페이지 준비중입니다...</p>
-          <p>현재 OS: {os}</p>
-        </span>
-      }
-    />
-  )
-  // if (device === 'mobile') {
-  //   return (
-  //     <SeeOther
-  //       description={
-  //         <span>
-  //           <p>페이지 준비중입니다...</p>
-  //           <p>현재 OS: {os}</p>
-  //         </span>
-  //       }
-  //     />
-  //   )
-  // }
+  if (device === 'mobile') {
+    return (
+      <SeeOther
+        title={
+          <Image
+            src="/static/plat-logo-nobg.svg"
+            alt="plat-logo"
+            width={40}
+            height={40}
+          />
+        }
+        description={
+          <span>
+            <p>페이지 준비중입니다...</p>
+            <p>현재 OS: {os}</p>
+          </span>
+        }
+      />
+    )
+  }
 
-  // if (device === 'desktop') {
-  //   return (
-  //     <Geolocation options={options.geolocation}>
-  //       {({ isLoading, position, error }) => (
-  //         <Map
-  //           isLoading={isLoading}
-  //           fallback={<Loading />}
-  //           error={error && new Error(error.message)}
-  //           apiType="kakao"
-  //           apiKey={options.kakao.apiKey}
-  //           location={
-  //             position && [position.coords.latitude, position.coords.longitude]
-  //           }
-  //           zoom={options.kakao.zoom}
-  //           className="w-full h-screen"
-  //         />
-  //       )}
-  //     </Geolocation>
-  //   )
-  // }
-  // throw new Error('맵 로딩 중에 에러가 발생했습니다')
+  if (device === 'desktop') {
+    return (
+      <Geolocation options={options.geolocation}>
+        {({ isLoading, position, error }) => (
+          <Map
+            isLoading={isLoading}
+            fallback={<Loading />}
+            error={error && new Error(error.message)}
+            apiType="kakao"
+            apiKey={options.kakao.apiKey}
+            location={
+              position && [position.coords.latitude, position.coords.longitude]
+            }
+            zoom={options.kakao.zoom}
+            className="w-full h-screen"
+          />
+        )}
+      </Geolocation>
+    )
+  }
+  throw new Error('맵 로딩 중에 에러가 발생했습니다')
 }
 
 export default MapService
